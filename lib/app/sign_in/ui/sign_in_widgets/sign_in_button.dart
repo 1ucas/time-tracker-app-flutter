08e1cb50b24db6e3d5b228b6537d1a554b317311
Lucas Maciel <lucasramos.m@gmail.com>
2020-06-14 23:34:16 -0300

    Configuração de arquivos

--- lib/app/sign_in/ui/sign_in_widgets/sign_in_button.dart
+++ lib/app/sign_in/ui/sign_in_widgets/sign_in_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:time_tracker_flutter/common_widgets/manobray_raised_button.dart';
+import 'package:time_tracker_flutter/common/ui/custom_widgets/manobray_raised_button.dart';
 
 class SignInButton extends ManobrayRaisedButton {
   SignInButton({
