29ca88f8c272a0ea910ed442d7e0e601dc9caee5
Lucas Maciel <lucasramos.m@gmail.com>
2020-06-17 00:12:13 -0300

    Trocando BLOC por ValueNotifier

--- lib/app/sign_in/domain/use_cases/sign_in_manager.dart
+++ lib/app/sign_in/domain/use_cases/sign_in_manager.dart
@@ -1,29 +1,22 @@
 
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:time_tracker_flutter/common/domain/models/user.dart';
 import 'package:time_tracker_flutter/common/domain/repositories/auth_repository.dart';
 
-class SignInBloc {
-  SignInBloc(this.auth);
+class SignInManager {
+  SignInManager({@required this.auth, @required this.isLoading});
   
   final AuthRepository auth;
-  final _isLoadingController = StreamController<bool>();
-
-  get isLoadingStream => _isLoadingController.stream;
-
-  void dispose() {
-    _isLoadingController.close();
-  }
-
-  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  final ValueNotifier<bool> isLoading;
 
   Future<User> _signIn(Future<User> Function() signInMethod) async {
     try {
-      _setIsLoading(true);
+      isLoading.value = true;
       return await signInMethod();
     } catch (e) {
-      _setIsLoading(false);
+      isLoading.value = false;
       rethrow;
     }
   }
