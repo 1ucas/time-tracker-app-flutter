08c4f1a190b23397cfe4a4bc6d71c9133320f535
Lucas Maciel <lucasramos.m@gmail.com>
2020-06-24 15:47:15 -0300

    Teste da Landing Page usando Mock de Stream

---
+++ test/landing_page_test.dart
@@ -0,0 +1,75 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter/app/home/ui/home_page.dart';
+import 'package:time_tracker_flutter/app/landing/ui/landing_page.dart';
+import 'package:time_tracker_flutter/app/sign_in/ui/selection_sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter/common/domain/models/user.dart';
+import 'package:time_tracker_flutter/common/domain/repositories/auth_repository.dart';
+
+class MockAuth extends Mock implements AuthRepository {}
+
+void main() {
+  MockAuth mockAuth;
+  StreamController<User> authStateController;
+
+  setUp(() {
+    mockAuth = MockAuth();
+    authStateController = StreamController<User>();
+  });
+
+  tearDown(() {
+    authStateController.close();
+  });
+
+  Future<void> pumpLandingPage(WidgetTester tester) async {
+    await tester.pumpWidget(
+      Provider<AuthRepository>(
+        create: (_) => mockAuth,
+        child: MaterialApp(
+          home: LandingPage(),
+        ),
+      ),
+    );
+
+    // Widget pega o último valor após rebuild
+    await tester.pump();
+  }
+
+  void stubOnAuthStateChanged(Iterable<User> onAuthStateChanged) {
+    authStateController
+        .addStream(Stream<User>.fromIterable(onAuthStateChanged));
+    when(mockAuth.onAuthStateChanged).thenAnswer((_) {
+      return authStateController.stream;
+    });
+  }
+
+  testWidgets('stream waiting', (WidgetTester tester) async {
+    stubOnAuthStateChanged([]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('null user', (WidgetTester tester) async {
+    stubOnAuthStateChanged([null]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(SignInPage), findsOneWidget);
+  });
+
+  testWidgets('Usuário presente', (WidgetTester tester) async {
+    stubOnAuthStateChanged([
+      User(uid: '123'),
+    ]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(HomePage), findsOneWidget);
+  });
+}
